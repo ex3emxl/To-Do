57f3b7026d5982ab95c9efb785fbdb4881704f37
ex3emxl <ex3emxl@gmail.com>
2018-12-30 22:43:21 +0200

    added filter to the Task List component

--- src/components/tasklist/Tasklist.js
+++ src/components/tasklist/Tasklist.js
@@ -8,8 +8,11 @@ class Tasklist extends Component {
 
     userId = 3;
 
+    noResults = "No Results";
+
     state = {
-        todos: []
+        todos: [],
+        displayFilter: []
     };
 
     componentDidMount() {
@@ -31,34 +34,69 @@ class Tasklist extends Component {
             el => {
                 if (el.completed === false) {
                     el.id === key ? el.completed = true : el.completed = el.completed;
-                } else if(el.completed === true) {
+                } else if (el.completed === true) {
                     el.id === key ? el.completed = false : el.completed = el.completed;
                 }
                 return el
             }
         );
-        this.setState({ todos: userTasks });
+        this.setState({todos: userTasks});
         e.preventDefault();
+    }
+
+    handleInputChange = (e) => {
+        if (e.target.value.length > 2){
+            let displayFilter = this.state.todos.filter( task => task.title.includes(e.target.value) );
+            displayFilter = displayFilter.length ? displayFilter : this.noResults;
+            this.setState({ displayFilter: displayFilter })
+
+        } else {
+            this.setState({ displayFilter: [] })
+        }
+        return true
     }
 
     render() {
 
-        const {todos} = this.state;
+        const {todos, displayFilter} = this.state;
+        const tasks = displayFilter.length ? displayFilter : todos;
 
-        return (
-            <ol className='tasklist'>
-                {
-                    todos.map(el => (
-                        <li className={el.completed ? 'completed' : ''} key={el.id} onClick={() => null}>
-                            {el.title}
-                            <a className="delete" onClick={this.deleteTask}><FaTrashAlt/></a>
-                            <a className="in-progress" onClick={this.inProgress}><FaBatteryThreeQuarters/></a>
-                            <a className={el.completed ? '' : 'complete'} onClick={(e) => this.completeTask(el.id, e)}><FaCheck/></a>
-                        </li>
-                    ))
-                }
-            </ol>
-        );
+        if (displayFilter === this.noResults) {
+
+            return (
+                <div>
+                    <label>Filter</label>
+                    <input onChange= { this.handleInputChange } type="text"/>
+                    <span>Need type more than 3 symbols</span>
+                    <ul className='tasklist'>
+                        { this.noResults }
+                    </ul>
+                </div>
+            );
+
+        } else {
+
+            return (
+                <div>
+                    <label>Filter</label>
+                    <input onChange= { this.handleInputChange } type="text"/>
+                    <span>Need type more than 3 symbols</span>
+                    <ol className='tasklist'>
+                        {
+                            tasks.map(el => (
+                                <li className={el.completed ? 'completed' : ''} key={el.id} onClick={() => null}>
+                                    {el.title}
+                                    <a className="delete" onClick={this.deleteTask}><FaTrashAlt/></a>
+                                    <a className="in-progress" onClick={this.inProgress}><FaBatteryThreeQuarters/></a>
+                                    <a className={el.completed ? '' : 'complete'}
+                                       onClick={(e) => this.completeTask(el.id, e)}><FaCheck/></a>
+                                </li>
+                            ))
+                        }
+                    </ol>
+                </div>
+            );
+        }
     }
 }
 
