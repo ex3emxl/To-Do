a56c4e00f4145a15615a52bcb445b845a51a020b
ex3emxl <ex3emxl@gmail.com>
2019-01-15 17:12:21 +0200

    refactor(component): deleted unnecessary code from Main component

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,25 +2,13 @@ import React from 'react';
 
 import Content from '../content';
 import Tasklist from '../Tasklist';
-import { Tabs, Tab } from '../tabs';
-import img from './logo.png';
 
 import './main.scss';
 
-const tabs = [
-    {title: 'Tab 1', content: 'Some text is here'},
-    {title: 'Tab 2', content: 'Another content'},
-    {title: 'Tab 3', content: 'Third text'}
-];
-
-
 const Main = () => (
     <main className='main'>
-        {/*<img src={img}/>*/}
         <Content />
         <Tasklist />
-
-        {/*<Tabs tabs={tabs}/>*/}
     </main>
 );
 
